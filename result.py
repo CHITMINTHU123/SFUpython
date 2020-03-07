61dbaceb2181cee19e561319b0a77f7acfd0e426
CHITMINTHU123 <mrchitminthu@icloud.com>
2020-03-07 12:07:05 +0630

    commit

---
+++ result.py
@@ -0,0 +1,18 @@
+import student
+import mary
+
+x = student.a["code"]
+y = student.b["code"]
+z = student.a["student"]
+print(x , ' ', y, ' ', z)
+
+i = student.a["mother"]
+print(i)
+
+for i in range(len(mary.a)):
+	print(i, mary.a[i])
+
+
+result.py
+
+#>>> File input/output
